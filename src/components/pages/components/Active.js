173e138fcbf24bec478e18644d5041589be10be3
kwonh <doublus@gmail.com>
2016-05-25 17:05:11 +0900

    DataGrid 표현을 위한 모듈 추가 및 Component 개발

--- src/components/pages/components/Active.js
+++ src/components/pages/components/Active.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Spreadsheet from '../../commons/Spreadsheet';
+
 class Active extends React.Component {
   render() {
     return(
       <div>
         <h2>Active</h2>
+        <Spreadsheet />
       </div>
     )
   }
