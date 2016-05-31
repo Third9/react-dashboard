ca76b31a1a9058f8d56d6e98dd835d7af58abe38
kwonh <doublus@gmail.com>
2016-05-31 11:03:08 +0900

    Login 화면 구성 및 코드 최적화

--- src/components/commons/Auth/Login.js
+++ src/components/commons/Auth/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import {withRouter} from 'react-router';
+
+import {Panel, FormControl, Button} from 'react-bootstrap';
 
 import {Auth} from './';
 
@@ -9,27 +10,60 @@ class Login extends React.Component {
     super(props);
 
     this.handleSubmit = this.handleSubmit.bind(this);
-    // this.requireAuth = this.requireAuth.bind(this);
+    this.updateAuth = this.updateAuth.bind(this);
+    this.changeLoginID = this.changeLoginID.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+
     this.auth = this.props.Auth;
     this.parent = this.props.parent;
 
     this.state = {
       loggedIn: this.auth.loggedIn(),
-      error: false
+      error: false,
+      loginID: 'test@test.com',
+      password: ''
     };
 
       console.log(`loggedIn: ${this.state.loggedIn}`)
   };
 
+  componentWillMount() {
+      this.auth.onChange = this.updateAuth
+      this.auth.login()
+  }
+
+  updateAuth(loggedIn) {
+    this.setState({
+      loggedIn: loggedIn
+    })
+  }
+
+  changeLoginID(evt) {
+    this.setState({
+      loginID: evt.target.value,
+      loginError: ''
+    });
+  }
+
+  changePassword(evt) {
+    this.setState({
+      password: evt.target.value,
+      loginError: ''
+    });
+  }
+
   handleSubmit(evt) {
     evt.preventDefault()
 
-    let email = this.refs.email.value
-    let pass = this.refs.pass.value
+    let email = this.state.loginID
+    let pass = this.state.password
 
     this.auth.login(email, pass, (loggedIn) => {
-      if (!loggedIn)
-        return this.setState({ loggedIn: true })
+      if (!loggedIn) {
+        alert('Login false')
+        return
+      }
+
 
       let { location } = this.parent
 
@@ -44,14 +78,28 @@ class Login extends React.Component {
   render() {
     return(
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label><input ref="email" placeholder="email" defaultValue="test@test.com" /></label>
-          <label><input ref="pass" placeholder="password" /></label> (hint: password1)<br />
-          <button type="submit">login</button>
-          {this.state.error && (
-            <p>Bad login information</p>
-          )}
-        </form>
+          <div className="col-md-4 col-md-offset-4">
+            <div className="text-center">
+              <h1 className="login-brand-text">RMS2 Login</h1>
+            </div>
+            <Panel header={<h3>Please Log In</h3>} className="login-panel">
+              <form role='form' onSubmit={this.handleSubmit}>
+                <fieldset>
+                  <div className="form-group">
+                    <FormControl onChange={this.changeLoginID} className="form-control" placeholder="Username"
+                    ref="loginID" type="email" autofocus="" name="name" defaultValue={this.state.loginID} />
+                  </div>
+
+                  <div className="form-group">
+                    <FormControl onChange={this.changePassword} placeholder="Password"
+                    ref="password" type="password" name="password" />
+                  </div>
+                  <FormControl type="checkbox" label="Remember Me" />
+                  <Button type="submit" bsSize="large" bsStyle="success" block>Login</Button>
+                </fieldset>
+              </form>
+            </Panel>
+          </div>
       </div>
     )
   }
